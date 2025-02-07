028e8f95c213469505ab19c77cad1c75348cf910
Priydarshini Karanth <kpriya1203@gmail.com>
2025-02-07 18:19:03 +0530

    basic frontend setup with shadcn and tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl border border-red-600">Event Management App</h1>
+      <Button>Click ME</Button>
+    </>
+  );
+}
+
+export default App;
